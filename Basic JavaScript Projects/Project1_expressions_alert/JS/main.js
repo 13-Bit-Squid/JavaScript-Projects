92873adf45d8122a5906fd20bd66bc97ae8d6acf
13-Bit-Squid <pendragreen@gmail.com>
2021-04-07 16:15:12 -0600

    additional comment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,18 +6,18 @@ var A = "This is a string!"; //Assigning a string to a variable.
 
 document.write(A);
 
-document.write("He said \"Son when, you grow up..." + "Something, something... Black Parade\"");
+document.write("He said \"Son when, you grow up..." + "Something, something... Black Parade\""); //Concatenating a string.
 
-var B = "This is" + " a complete sentence.";
+var B = "This is" + " a complete sentence."; //Concatenating a variable. 
 document.write(B);
 
-var person1 = "Ena", person2 = "Moony", person3 = "Turron";
+var person1 = "Ena", person2 = "Moony", person3 = "Turron"; //Assigning multiple variables differing values.
 document.write(person1);
 
-var person1 = person1.fontcolor("blue");
+var person1 = person1.fontcolor("blue"); //Assigning variables colors.
 
 var person2 = person2.fontcolor("silver");
 
 var person3 = person3.fontcolor("orange");
 
-42 + 56
+42 + 56 //This is an expression.
