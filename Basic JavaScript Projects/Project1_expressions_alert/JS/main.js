a8c19df289026ce81fc39fb7705c5532431585a9
13-Bit-Squid <pendragreen@gmail.com>
2021-04-07 16:18:24 -0600

    add var concatenating

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -8,16 +8,17 @@ document.write(A);
 
 document.write("He said \"Son when, you grow up..." + "Something, something... Black Parade\""); //Concatenating a string.
 
-var B = "This is" + " a complete sentence."; //Concatenating a variable. 
-document.write(B);
+var B = " I'm concatenating this string!"; //Concatenating a variable. 
+document.write(A+B);
 
 var person1 = "Ena", person2 = "Moony", person3 = "Turron"; //Assigning multiple variables differing values.
-document.write(person1);
 
 var person1 = person1.fontcolor("blue"); //Assigning variables colors.
 
 var person2 = person2.fontcolor("silver");
 
 var person3 = person3.fontcolor("orange");
+
+document.write(person1);
 
 42 + 56 //This is an expression.
