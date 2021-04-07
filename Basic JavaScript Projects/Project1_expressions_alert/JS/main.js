a5d7e0524b7298b5ce5dbbbe7fdf961061a1d357
13-Bit-Squid <pendragreen@gmail.com>
2021-04-07 16:11:23 -0600

    new stuff

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+window.alert("Hello, world!");
+
+document.write('Bonjour!');
+
+var A = "This is a string!"; //Assigning a string to a variable.
+
+document.write(A);
+
+document.write("He said \"Son when, you grow up..." + "Something, something... Black Parade\"");
+
+var B = "This is" + " a complete sentence.";
+document.write(B);
+
+var person1 = "Ena", person2 = "Moony", person3 = "Turron";
+document.write(person1);
+
+var person1 = person1.fontcolor("blue");
+
+var person2 = person2.fontcolor("silver");
+
+var person3 = person3.fontcolor("orange");
+
+42 + 56
