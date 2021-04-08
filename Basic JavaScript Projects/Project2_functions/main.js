af98b1b2606501b85bbe989f1d1794ae0cca3f5f
13-Bit-Squid <pendragreen@gmail.com>
2021-04-07 18:35:48 -0600

    comments

--- Basic JavaScript Projects/Project2_functions/main.js
+++ Basic JavaScript Projects/Project2_functions/main.js
@@ -1,10 +1,10 @@
-function Two_Var_Function() {
+function Two_Var_Function() { //getelementbyid
     var string = "This is a string!";
     var result = string.fontcolor("red")
     document.getElementById("Red_String").innerHTML = result;
 }
 
-function Var_Function() {
+function Var_Function() { //Concatenating 
     var sentence = "This is a string...";
     sentence += "Additional Text.";
     document.getElementById("Concatenate").innerHTML = sentence;
