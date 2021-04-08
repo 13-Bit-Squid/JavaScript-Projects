45058218143f941a0548f808218309042985cfbb
13-Bit-Squid <pendragreen@gmail.com>
2021-04-08 14:53:26 -0600

    Update main.js

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
-function my_Dictionary() {
+function my_Dictionary() { //Creating a series of KVP's.
     var Game = {
-        Title:"Mariokart",
+        Title:"Mariokart", //A game where I'm sure many friendships were ended.
         Genre:"Racing",
         Age_Rating:"E",
         Players:"1-8"
     };
-    delete Game.Title
+    delete Game.Title //Deleting the title of the game to get an undefined response.
     document.getElementById("Dictionary").innerHTML = Game.Title;
 }
